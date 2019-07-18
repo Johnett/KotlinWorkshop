f6af99a53e6abb4955b785d51a74ed345968ce64
Johnettdigitxp <johnett.mathew@digitxp.com>
2019-07-18 14:58:29 +0530

    some more leakcanary junk files

--- app/src/main/java/com/architecture/www/xmlshowcase/XmlShowCaseApplication.kt
+++ app/src/main/java/com/architecture/www/xmlshowcase/XmlShowCaseApplication.kt
@@ -2,12 +2,10 @@ package com.architecture.www.xmlshowcase
 
 import android.app.Application
 import android.os.StrictMode
-import android.view.View
 import com.bugsnag.android.Bugsnag
-import leakcanary.LeakCanary
 
 open class XmlShowCaseApplication : Application() {
-    val leakedViews = mutableListOf<View>()
+//    val leakedViews = mutableListOf<View>()
     override fun onCreate() {
         super.onCreate()
         Bugsnag.init(this)

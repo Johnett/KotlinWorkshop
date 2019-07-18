e23461a5b10f960ce58cd7cf2356166d56c77865
Johnettdigitxp <johnettmathew@gmail.com>
2019-07-18 15:53:34 +0530

    fixes suggested by codeacy plugin

--- app/src/main/java/com/architecture/www/xmlshowcase/ViewProperties.kt
+++ app/src/main/java/com/architecture/www/xmlshowcase/ViewProperties.kt
@@ -2,12 +2,21 @@ package com.architecture.www.xmlshowcase
 
 import android.view.View
 
+/**
+ * This function is used to set the visibility of passing view to visible
+ * */
 fun View.show() {
     visibility = View.VISIBLE
 }
 
+/**
+ * This function is used to set the visibility of the passing view to gone
+ * */
 fun View.hide() {
     visibility = View.GONE
 }
 
+/**
+ * This function is used to set the visibility of the passing view to visible or gone
+ * */
 fun View.showOrHide(show: Boolean) = if (show) show() else hide()
